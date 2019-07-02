3d32cc6372a761a99187cefcdad865b0b9b8f7a3
Joshi-Karthik <47510583+Joshi-Karthik@users.noreply.github.com>
2019-07-02 22:08:16 +1000

    Initial commit

---
+++ election.js
@@ -0,0 +1,850 @@
+var turnout = {
+  "AN": {
+    "State": "Andaman & Nicobar Islands",
+		"capital": {
+			"name": "Port Blair",
+			"details": [
+				{
+					"name": "Port Blair",
+					"coordinates": [ 11.623377, 92.726483 ],
+					"population": "100,608",
+					"sexratio": 889,
+					"literacy": 89.76
+				}
+		]},
+    "MaleElectors": 142782,
+    "MaleVoters": 101178,
+    "MaleTurnout": 70,
+    "FemaleElectors": 126578,
+    "FemaleVoters": 89150,
+    "FemaleTurnout": 70.43,
+    "TotalElectors": 269360,
+    "TotalVoters": 190328,
+    "Value": 40,
+    "TotalSeats": 1,
+    "Party": "BJP",
+    "Sentiments": 100,
+    "SexRatio": 88
+  },
+  "AP": {
+    "State": "Andhra Pradesh",
+	"capital": {
+			"name": "Hyderabad",
+			"details": [
+				{
+					"name": "Hyderabad",
+					"coordinates": [ 17.385044, 78.486671 ],
+					"population": "7,749,334",
+					"sexratio": 945,
+					"literacy": 82.96
+				}
+			]
+		},
+    "MaleElectors": 32670930,
+    "MaleVoters": 24352913,
+    "MaleTurnout": 74.54,
+    "FemaleElectors": 32267820,
+    "FemaleVoters": 24005632,
+    "FemaleTurnout": 74.39,
+    "TotalElectors": 64938750,
+    "TotalVoters": 48358545,
+    "Value": 74.47,
+    "TotalSeats": 42,
+    "Party": "TDP",
+    "Sentiments": 29252,
+    "SexRatio": 99
+  },
+  "AR": {
+    "State": "Arunachal Pradesh",
+	"capital": {
+			"name": "Itanagar",
+			"details": [
+				{
+					"name": "Itanagar",
+					"coordinates": [ 27.084368, 93.605316 ]
+				}
+			]
+		},
+    "MaleElectors": 379627,
+    "MaleVoters": 289291,
+    "MaleTurnout": 76.2,
+    "FemaleElectors": 379760,
+    "FemaleVoters": 307665,
+    "FemaleTurnout": 81.02,
+    "TotalElectors": 759387,
+    "TotalVoters": 596956,
+    "Value": 78.61,
+    "TotalSeats": 2,
+    "Party": "BJP",
+    "Sentiments": 2180,
+    "SexRatio": 106
+  },
+  "AS": {
+    "State": "Assam",
+	"capital": {
+			"name": "Dispur",
+			"details": [
+				{
+					"name": "Dispur",
+					"coordinates": [ 26.140833, 91.790833 ]
+				}
+	]},
+    "MaleElectors": 9787209,
+    "MaleVoters": 7876763,
+    "MaleTurnout": 80.48,
+    "FemaleElectors": 9098065,
+    "FemaleVoters": 7209120,
+    "FemaleTurnout": 79.24,
+    "TotalElectors": 18885274,
+    "TotalVoters": 15085883,
+    "Value": 79.88,
+    "TotalSeats": 14,
+    "Party": "BJP",
+    "Sentiments": 1142,
+    "SexRatio": 92
+  },
+  "BR": {
+    "State": "Bihar",
+	"capital": {
+			"name": "Patna",
+			"details": [
+				{
+					"name": "Patna",
+					"coordinates": [ 25.611000, 85.144000 ],
+					"population": "2,046,652",
+					"sexratio": 882,
+					"literacy": 84.71
+				}
+			]
+		},
+    "MaleElectors": 34092938,
+    "MaleVoters": 18779230,
+    "MaleTurnout": 39,
+    "FemaleElectors": 29668858,
+    "FemaleVoters": 17106136,
+    "FemaleTurnout": 57.66,
+    "TotalElectors": 63761796,
+    "TotalVoters": 35885366,
+    "Value": 56.28,
+    "TotalSeats": 40,
+    "Party": "BJP",
+    "Sentiments": 3577,
+    "SexRatio": 91
+  },
+  "CT": {
+    "State": "Chhattisgarh",
+	"capital": {
+			"name": "Raipur",
+			"details": [
+				{
+					"name": "Raipur",
+					"coordinates": [ 21.251384, 81.629641 ],
+					"population": "1,122,555",
+					"sexratio": 945,
+					"literacy": 86.90
+				}
+			]
+		},
+    "MaleElectors": 8915783,
+    "MaleVoters": 6320693,
+    "MaleTurnout": 70.89,
+    "FemaleElectors": 8707266,
+    "FemaleVoters": 5934886,
+    "FemaleTurnout": 68.16,
+    "TotalElectors": 17623049,
+    "TotalVoters": 12255579,
+    "Value": 69.54,
+    "TotalSeats": 11,
+    "Party": "BJP",
+    "Sentiments": 318,
+    "SexRatio": 94
+  },
+  "GA": {
+    "State": "Goa",
+	"capital": {
+			"name": "Panaji",
+			"details": [
+				{
+					"name": "Panaji",
+					"coordinates": [ 15.490930, 73.827850 ]
+				}
+			]
+		},
+    "MaleElectors": 528308,
+    "MaleVoters": 395766,
+    "MaleTurnout": 74.91,
+    "FemaleElectors": 532469,
+    "FemaleVoters": 421234,
+    "FemaleTurnout": 79.11,
+    "TotalElectors": 1060777,
+    "TotalVoters": 817000,
+    "Value": 77.02,
+    "TotalSeats": 2,
+    "Party": "BJP",
+    "Sentiments": 13,
+    "SexRatio": 106
+  },
+  "GJ": {
+    "State": "Gujarat",
+	"capital": {
+			"name": "Gandhinagar",
+			"details": [
+				{
+					"name": "Gandhinagar",
+					"coordinates": [ 23.215635, 72.636941 ],
+					"population": "208,299",
+					"sexratio": 903,
+					"literacy": 93.70
+				}
+			]
+		},
+    "MaleElectors": 21229092,
+    "MaleVoters": 14259115,
+    "MaleTurnout": 45,
+    "FemaleElectors": 19374012,
+    "FemaleVoters": 11564888,
+    "FemaleTurnout": 59.69,
+    "TotalElectors": 40603104,
+    "TotalVoters": 25824003,
+    "Value": 63.6,
+    "TotalSeats": 26,
+    "Party": "BJP",
+    "Sentiments": 9160,
+    "SexRatio": 81
+  },
+  "HR": {
+    "State": "Haryana",
+	"capital": {
+			"name": "Chandigarh",
+			"details": [
+				{
+					"name": "Chandigarh",
+					"coordinates": [ 30.733315, 76.779418 ],
+					"population": "1,025,682",
+					"sexratio": 829,
+					"literacy": 86.77
+				}
+			]
+		},
+    "MaleElectors": 8716612,
+    "MaleVoters": 6351584,
+    "MaleTurnout": 72.87,
+    "FemaleElectors": 7381137,
+    "FemaleVoters": 5143566,
+    "FemaleTurnout": 69.69,
+    "TotalElectors": 16097749,
+    "TotalVoters": 11495150,
+    "Value": 71.41,
+    "TotalSeats": 10,
+    "Party": "BJP",
+    "Sentiments": 333,
+    "SexRatio": 81
+  },
+  "HP": {
+    "State": "Himachal Pradesh",
+	"capital": {
+			"name": "Shimla",
+			"details": [
+				{
+					"name": "Shimla",
+					"coordinates": [ 31.104814, 77.173403 ],
+					"population": "171,817",
+					"sexratio": 818,
+					"literacy": 94.67
+				}
+			]
+		},
+    "MaleElectors": 2474430,
+    "MaleVoters": 1569632,
+    "MaleTurnout": 63.43,
+    "FemaleElectors": 2335641,
+    "FemaleVoters": 1528869,
+    "FemaleTurnout": 65.46,
+    "TotalElectors": 4810071,
+    "TotalVoters": 3098501,
+    "Value": 64.42,
+    "TotalSeats": 4,
+    "Party": "BJP",
+    "Sentiments": 1993,
+    "SexRatio": 97
+  },
+  "JK": {
+    "State": "Jammu & Kashmir",
+	"capital": {
+			"name": "Srinagar(summer), Jammu(winter)",
+			"details": [
+				{
+					"name": "Srinagar (summer)",
+					"coordinates": [ 34.083658, 74.797368 ],
+					"population": "1,273,312",
+					"sexratio": 888,
+					"literacy": 70.98
+				},
+				{
+					"name": "Jammu (winter)",
+					"coordinates": [ 32.726602, 74.857026 ],
+					"population": "651,826",
+					"sexratio": 898,
+					"literacy": 89.66
+				}
+			]
+		},
+    "MaleElectors": 3802021,
+    "MaleVoters": 1927584,
+    "MaleTurnout": 50.7,
+    "FemaleElectors": 3400142,
+    "FemaleVoters": 1639279,
+    "FemaleTurnout": 48.21,
+    "TotalElectors": 7202163,
+    "TotalVoters": 3566863,
+    "Value": 49.52,
+    "TotalSeats": 6,
+    "Party": "BJP",
+    "Sentiments": 580,
+    "SexRatio": 85
+  },
+  "JH": {
+    "State": "Jharkhand",
+	"capital": {
+			"name": "Ranchi",
+			"details": [
+				{
+					"name": "Ranchi",
+					"coordinates": [ 23.344100, 85.309562 ],
+					"population": "1,126,741",
+					"sexratio": 920,
+					"literacy": 88.49
+				}
+			]
+		},
+    "MaleElectors": 10685149,
+    "MaleVoters": 6860476,
+    "MaleTurnout": 64.21,
+    "FemaleElectors": 9641594,
+    "FemaleVoters": 6122464,
+    "FemaleTurnout": 63.5,
+    "TotalElectors": 20326743,
+    "TotalVoters": 12982940,
+    "Value": 63.87,
+    "TotalSeats": 14,
+    "Party": "BJP",
+    "Sentiments": 2644,
+    "SexRatio": 89
+  },
+  "KA": {
+    "State": "Karnataka",
+	"capital": {
+			"name": "Bengaluru",
+			"details": [
+				{
+					"name": "Bengaluru",
+					"coordinates": [ 12.971599, 77.594563 ],
+					"population": "8,499,399",
+					"sexratio": 914,
+					"literacy": 89.59
+				}
+			]
+		},
+    "MaleElectors": 23586223,
+    "MaleVoters": 16165832,
+    "MaleTurnout": 68.54,
+    "FemaleElectors": 22625886,
+    "FemaleVoters": 14873056,
+    "FemaleTurnout": 65.73,
+    "TotalElectors": 46212109,
+    "TotalVoters": 31038888,
+    "Value": 67.17,
+    "TotalSeats": 28,
+    "Party": "INC",
+    "Sentiments": 10233,
+    "SexRatio": 92
+  },
+  "KL": {
+    "State": "Kerala",
+	"capital": {
+			"name": "Thiruvanandhapuram",
+			"details": [
+				{
+					"name": "Thiruvanandhapuram",
+					"coordinates": [ 8.524139, 76.936638 ],
+					"population": "1,687,406",
+					"sexratio": 1064,
+					"literacy": 93.72
+				}
+			]
+		},
+    "MaleElectors": 11734258,
+    "MaleVoters": 8678185,
+    "MaleTurnout": 80,
+    "FemaleElectors": 12592391,
+    "FemaleVoters": 9297708,
+    "FemaleTurnout": 73.84,
+    "TotalElectors": 24326649,
+    "TotalVoters": 17975893,
+    "Value": 73.89,
+    "TotalSeats": 20,
+    "Party": "UDF",
+    "Sentiments": 1167,
+    "SexRatio": 107
+  },
+  "LD": {
+    "State": "Lakshadweep",
+	"capital": {
+			"name": "Kavaratti",
+			"details": [
+				{
+					"name": "Kavaratti",
+					"coordinates": [ 10.559320, 72.635813 ]
+				}
+			]
+		},
+    "MaleElectors": 25433,
+    "MaleVoters": 21585,
+    "MaleTurnout": 84.87,
+    "FemaleElectors": 24489,
+    "FemaleVoters": 21654,
+    "FemaleTurnout": 88.42,
+    "TotalElectors": 49922,
+    "TotalVoters": 43239,
+    "Value": 86.61,
+    "TotalSeats": 1,
+    "Party": "INC",
+    "Sentiments": 15,
+    "SexRatio": 100
+  },
+  "MP": {
+    "State": "Madhya Pradesh",
+	"capital": {
+			"name": "Bhopal",
+			"details": [
+				{
+					"name": "Bhopal",
+					"coordinates": [ 23.259933, 77.412615 ],
+					"population": "1,883,381",
+					"sexratio": 911,
+					"literacy": 85.24
+				}
+			]
+		},
+    "MaleElectors": 25309830,
+    "MaleVoters": 16734556,
+    "MaleTurnout": 66.12,
+    "FemaleElectors": 22808210,
+    "FemaleVoters": 12905240,
+    "FemaleTurnout": 56.58,
+    "TotalElectors": 48118040,
+    "TotalVoters": 29639796,
+    "Value": 61.6,
+    "TotalSeats": 29,
+    "Party": "BJP",
+    "Sentiments": 1701,
+    "SexRatio": 77
+  },
+  "MH": {
+    "State": "Maharashtra",
+	"capital": {
+			"name": "Mumbai",
+			"details": [
+				{
+					"name": "Mumbai",
+					"coordinates": [ 19.075984, 72.877656 ],
+					"population": "18,414,288",
+					"sexratio": 852,
+					"literacy": 90.28
+				}
+			]
+		},
+	"MaleElectors": 42743156,
+    "MaleVoters": 26714578,
+    "MaleTurnout": 62.5,
+    "FemaleElectors": 37974127,
+    "FemaleVoters": 22004266,
+    "FemaleTurnout": 57.95,
+    "TotalElectors": 80717283,
+    "TotalVoters": 48718844,
+    "Value": 60.36,
+    "TotalSeats": 48,
+    "Party": "BJP",
+    "Sentiments": 12319,
+    "SexRatio": 82
+  },
+  "MN": {
+    "State": "Manipur",
+	"capital": {
+			"name": "Imphal",
+			"details": [
+				{
+					"name": "Imphal",
+					"coordinates": [ 24.820000, 93.950000 ],
+					"population": "414,288",
+					"sexratio": 1055,
+					"literacy": 91.70
+				}
+			]
+		},
+    "MaleElectors": 871431,
+    "MaleVoters": 685427,
+    "MaleTurnout": 85,
+    "FemaleElectors": 902894,
+    "FemaleVoters": 727210,
+    "FemaleTurnout": 80.54,
+    "TotalElectors": 1774325,
+    "TotalVoters": 1412637,
+    "Value": 79.62,
+    "TotalSeats": 2,
+    "Party": "INC",
+    "Sentiments": 748,
+    "SexRatio": 106
+  },
+  "ML": {
+    "State": "Meghalaya",
+	"capital": {
+			"name": "Shillong",
+			"details": [
+				{
+					"name": "Shillong",
+					"coordinates": [ 25.578773, 91.893254 ],
+					"population": "354,325",
+					"sexratio": 1042,
+					"literacy": 92.34
+				}
+			]
+		},
+    "MaleElectors": 777639,
+    "MaleVoters": 524774,
+    "MaleTurnout": 67.48,
+    "FemaleElectors": 789602,
+    "FemaleVoters": 553284,
+    "FemaleTurnout": 70.07,
+    "TotalElectors": 1567241,
+    "TotalVoters": 1078058,
+    "Value": 68.79,
+    "TotalSeats": 2,
+    "Party": "INC",
+    "Sentiments": 120,
+    "SexRatio": 105
+  },
+  "MZ": {
+    "State": "Mizoram",
+	"capital": {
+			"name": "Aizawl",
+			"details": [
+				{
+					"name": "Aizawl",
+					"coordinates": [ 23.727107, 92.717639 ],
+					"population": "291,822",
+					"sexratio": 1029,
+					"literacy": 98.80
+				}
+			]
+		},
+    "MaleElectors": 346219,
+    "MaleVoters": 216167,
+    "MaleTurnout": 62.44,
+    "FemaleElectors": 355951,
+    "FemaleVoters": 217034,
+    "FemaleTurnout": 60.97,
+    "TotalElectors": 702170,
+    "TotalVoters": 433201,
+    "Value": 61.69,
+    "TotalSeats": 1,
+    "Party": "INC",
+    "Sentiments": 240,
+    "SexRatio": 100
+  },
+  "NL": {
+    "State": "Nagaland",
+	"capital": {
+			"name": "Kohima",
+			"details": [
+				{
+					"name": "Kohima",
+					"coordinates": [ 25.670100, 94.107700 ]
+				}
+			]
+		},
+    "MaleElectors": 600490,
+    "MaleVoters": 529325,
+    "MaleTurnout": 88.15,
+    "FemaleElectors": 582458,
+    "FemaleVoters": 509585,
+    "FemaleTurnout": 87.49,
+    "TotalElectors": 1182948,
+    "TotalVoters": 1038910,
+    "Value": 87.82,
+    "TotalSeats": 1,
+    "Party": "INC",
+    "Sentiments": 230,
+    "SexRatio": 96
+  },
+  "DL": {
+    "State": "NCT OF Delhi",
+    "MaleElectors": 7051098,
+    "MaleVoters": 4653522,
+    "MaleTurnout": 70,
+    "FemaleElectors": 5660138,
+    "FemaleVoters": 3618244,
+    "FemaleTurnout": 63.93,
+    "TotalElectors": 12711236,
+    "TotalVoters": 8271766,
+    "Value": 65.07,
+    "TotalSeats": 7,
+    "Party": "BJP",
+    "Sentiments": 11963,
+    "SexRatio": 78
+  },
+  "OR": {
+    "State": "Odisha",
+	"capital": {
+			"name": "Bhubaneshwar",
+			"details": [
+				{
+					"name": "Bhubaneshwar",
+					"coordinates": [ 20.296059, 85.824540 ],
+					"population": "881,988",
+					"sexratio": 882,
+					"literacy": 93.15
+				}
+			]
+		},
+    "MaleElectors": 15194309,
+    "MaleVoters": 11032523,
+    "MaleTurnout": 72.61,
+    "FemaleElectors": 14001732,
+    "FemaleVoters": 10499752,
+    "FemaleTurnout": 74.99,
+    "TotalElectors": 29196041,
+    "TotalVoters": 21532275,
+    "Value": 73.75,
+    "TotalSeats": 21,
+    "Party": "BJP",
+    "Sentiments": 1638,
+    "SexRatio": 95
+  },
+  "PB": {
+    "State": "Punjab",
+	"capital": {
+			"name": "Chandigarh",
+			"details": [
+				{
+					"name": "Chandigarh",
+					"coordinates": [ 30.733315, 76.779418 ],
+					"population": "1,025,682",
+					"sexratio": 829,
+					"literacy": 86.77
+				}
+			]
+		},
+    "MaleElectors": 10327116,
+    "MaleVoters": 7262625,
+    "MaleTurnout": 70.33,
+    "FemaleElectors": 9280892,
+    "FemaleVoters": 6582507,
+    "FemaleTurnout": 70.93,
+    "TotalElectors": 19608008,
+    "TotalVoters": 13845132,
+    "Value": 70.61,
+    "TotalSeats": 13,
+    "Party": "INC",
+    "Sentiments": 14416,
+    "SexRatio": 91
+  },
+  "RJ": {
+    "State": "Rajasthan",
+	"capital": {
+			"name": "Jaipur",
+			"details": [
+				{
+					"name": "Jaipur",
+					"coordinates": [ 26.912434, 75.787271 ],
+					"population": "3,073,350",
+					"sexratio": 898,
+					"literacy": 84.34
+				}
+			]
+		},
+    "MaleElectors": 22638323,
+    "MaleVoters": 14627976,
+    "MaleTurnout": 64.62,
+    "FemaleElectors": 20331124,
+    "FemaleVoters": 12482068,
+    "FemaleTurnout": 61.39,
+    "TotalElectors": 42969447,
+    "TotalVoters": 27110044,
+    "Value": 63.09,
+    "TotalSeats": 25,
+    "Party": "BJP",
+    "Sentiments": 250,
+    "SexRatio": 85
+  },
+  "SK": {
+    "State": "Sikkim",
+	"capital": {
+			"name": "Gangtok",
+			"details": [
+				{
+					"name": "Gangtok",
+					"coordinates": [ 27.338936, 88.606504 ]
+				}
+			]
+		},
+    "MaleElectors": 190886,
+    "MaleVoters": 158222,
+    "MaleTurnout": 82.89,
+    "FemaleElectors": 179725,
+    "FemaleVoters": 150745,
+    "FemaleTurnout": 83.88,
+    "TotalElectors": 370611,
+    "TotalVoters": 308967,
+    "Value": 83.37,
+    "TotalSeats": 1,
+    "Party": "BJP",
+    "Sentiments": 130,
+    "SexRatio": 95
+  },
+  "TN": {
+    "State": "Tamil Nadu",
+	"capital": {
+			"name": "Chennai",
+			"details": [
+				{
+					"name": "Chennai",
+					"coordinates": [ 13.082680, 80.270718 ],
+					"population": "8,696,010",
+					"sexratio": 986,
+					"literacy": 90.33
+				}
+			]
+		},
+    "MaleElectors": 27571785,
+    "MaleVoters": 20249949,
+    "MaleTurnout": 73.44,
+    "FemaleElectors": 27542720,
+    "FemaleVoters": 20370491,
+    "FemaleTurnout": 73.96,
+    "TotalElectors": 55114505,
+    "TotalVoters": 40620440,
+    "Value": 73.7,
+    "TotalSeats": 39,
+    "Party": "DMK",
+    "Sentiments": 7510,
+    "SexRatio": 101
+  },
+  "TR": {
+    "State": "Tripura",
+	"capital": {
+			"name": "Agartala",
+			"details": [
+				{
+					"name": "Agartala",
+					"coordinates": [ 23.831457, 91.286778 ],
+					"population": "399,688",
+					"sexratio": 1002,
+					"literacy": 93.88
+				}
+			]
+		},
+    "MaleElectors": 1217575,
+    "MaleVoters": 1035722,
+    "MaleTurnout": 85.06,
+    "FemaleElectors": 1171244,
+    "FemaleVoters": 988137,
+    "FemaleTurnout": 84.37,
+    "TotalElectors": 2388819,
+    "TotalVoters": 2023859,
+    "Value": 84.72,
+    "TotalSeats": 2,
+    "Party": "BJP",
+    "Sentiments": 127,
+    "SexRatio": 95
+  },
+  "UP": {
+    "State": "Uttar Pradesh",
+	"capital": {
+			"name": "Lucknow",
+			"details": [ 
+				{
+					"name": "Lucknow",
+					"coordinates": [ 26.846511, 80.946683 ],
+					"population": "2,901,474",
+					"sexratio": 915,
+					"literacy": 84.72
+				} 
+			] 
+		},
+    "MaleElectors": 76071444,
+    "MaleVoters": 44979619,
+    "MaleTurnout": 59.13,
+    "FemaleElectors": 62894376,
+    "FemaleVoters": 36112683,
+    "FemaleTurnout": 57.42,
+    "TotalElectors": 138965820,
+    "TotalVoters": 81092302,
+    "Value": 58.35,
+    "TotalSeats": 80,
+    "Party": "BJP",
+    "Sentiments": 6028,
+    "SexRatio": 80
+  },
+  "UT": {
+    "State": "Uttarakhand",
+	"capital": {
+			"name": "Lucknow",
+			"details": [ 
+				{
+					"name": "Lucknow",
+					"coordinates": [ 26.846511, 80.946683 ],
+					"population": "2,901,474",
+					"sexratio": 915,
+					"literacy": 84.72
+				} 
+			] 
+		},
+    "MaleElectors": 3751098,
+    "MaleVoters": 2268767,
+    "MaleTurnout": 60.48,
+    "FemaleElectors": 3378841,
+    "FemaleVoters": 2123123,
+    "FemaleTurnout": 62.84,
+    "TotalElectors": 7129939,
+    "TotalVoters": 4391890,
+    "Value": 61.66,
+    "TotalSeats": 5,
+    "Party": "BJP",
+    "Sentiments": 7663,
+    "SexRatio": 94
+  },
+  "WB": {
+    "State": "West Bengal",
+	"capital": {
+			"name": "Kolkata",
+			"details": [
+				{
+					"name": "Kolkata",
+					"coordinates": [ 22.572646, 88.363895 ],
+					"population": "14,112,536",
+					"sexratio": 899,
+					"literacy": 87.14
+				}
+			]
+		},
+    "MaleElectors": 32689449,
+    "MaleVoters": 26886706,
+    "MaleTurnout": 82.25,
+    "FemaleElectors": 30143679,
+    "FemaleVoters": 24735849,
+    "FemaleTurnout": 82.06,
+    "TotalElectors": 62833128,
+    "TotalVoters": 51622555,
+    "Value": 82.16,
+    "TotalSeats": 42,
+    "Party": "TMC",
+    "Sentiments": 4706,
+    "SexRatio": 92
+  }
+};
+
+  var Indiaturnout = {
+
+	states: turnout
+};
